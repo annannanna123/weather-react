a3756caf51d9aef8ac1f5d7abeb5542c4f77f197
annannanna123 <132966645+annannanna123@users.noreply.github.com>
2023-10-22 20:20:36 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="container">
         <h1>Weather App</h1>
         <Search />
-      </header>
-      <footer>
-        <a
-          href="https://github.com/annannanna123/weather-react"
-          target="blank_"
-        >
-          Open-source code
-        </a>{" "}
-        by Anna Chu
-      </footer>
+        <footer>
+          <a
+            href="https://github.com/annannanna123/weather-react"
+            target="blank_"
+          >
+            Open-source code
+          </a>{" "}
+          by Anna Chu
+        </footer>
+      </div>
     </div>
   );
 }
